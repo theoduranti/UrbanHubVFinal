2e846e1d77841269e92b81856ab29254387df812
Théo Duranti <theoduranti@hotmail.fr>
2018-06-14 16:08:25 +0200

    config stripe key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  publishable_key: Rails.application.secrets.stripe_publishable_key,
-  secret_key:      Rails.application.secrets.stripe_secret_key
+  :publishable_key => 'pk_test_geufYBE595yJfHOBtx1ukSGt',
+  :secret_key      => 'sk_test_04w1amZyr5HXuQwaUqvN9nNU'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
